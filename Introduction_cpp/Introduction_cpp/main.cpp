3fd728c4dbb90ddf37a2c1fe2923fb8e7102df22
Lehmann Nicolas <nico.nglconsulting@gmail.com>
2020-10-05 17:26:22 +0200

    Création d'un tableau de point

--- Introduction_cpp/Introduction_cpp/main.cpp
+++ Introduction_cpp/Introduction_cpp/main.cpp
@@ -7,32 +7,17 @@ using namespace std;
 int main() {
 
 
-	string strTypePoint = "shhshd";
+	
 	//Instance de classe CPoint
 	CPoint pt; //appel du constructeur sans parametres
 
-	CPoint pt2(8, 9,strTypePoint); //appel du constructeur avec parametres
+	CPoint pt2(8, 9,"pivot"); //appel du constructeur avec parametres
 	
-	int n_X;
-	int n_Y;
+	CPoint tabPoint[100];
 
-	char car = strTypePoint[1];
-
-
-	//ou std::cout
-	cout << "Entrez le premier entier" << endl;
-	cin >> n_X;
-
-	//ou std::cout
-	cout << "Entrez le second entier" << endl;
-	cin >> n_Y;
-
-	pt2.setnX(n_X);
-	pt2.setnY(n_Y);
+	for (int i = 0; i < 100; i++)
+		tabPoint[i] = pt2;
 	
-	cout << "X : " << n_X << endl;
-	cout << "Y : " << n_Y << endl;
-
 
 
 
