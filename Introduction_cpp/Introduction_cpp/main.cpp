3887e360b3885c210b5936a73236d6be8059e4ab
Lehmann Nicolas <nico.nglconsulting@gmail.com>
2021-01-25 16:51:32 +0100

    Les patrons de classes
    Création d'un patron de classes
    Contraintes d'utilisation d'un patron de classes
    Les paramètres de type
    Les paramètres expressions d'un patron de classes

--- Introduction_cpp/Introduction_cpp/main.cpp
+++ Introduction_cpp/Introduction_cpp/main.cpp
@@ -1,90 +1,27 @@
 #include <iostream>
-#include "CPoint.h"
 #include <string.h>
+#include "TCPoint.h"
 
 using namespace std;
 
 
-/*int minimum(int n_a, int n_b);
-float minimum(float flt_a, float flt_b);*/
-
-//prototype template
-template <class T> T minimum(T a, T b);
-const char* minimum(const char* adr1, const char* adr2);
-
-//template <class T, class U> fct(T a, T* b, U c);
-
 
 int main() {
 
-	minimum(10, 8);
-	minimum(12.f, 5.f);
+	//template classe TCPoint
+	TCPoint<int> pointInt(3, 8);
 
-	const char* adr1 = "hello";
-	const char* adr2 = "world";
+	TCPoint<double> pointDbl(3.f, 9.f);
 
-	cout << minimum(adr1, adr2) << endl;
+	pointInt.affiche();
 
-	CPoint pt(3,4);
-	CPoint pt2(1,2);
+	pointDbl.affiche();
 
-	cout << minimum(pt, pt2) << endl;
-
-
+	tableau <TCPoint<int>, 10> t;
 
 
 return 0;
 }
 
-/*template <class T, class U> fct(T a, T* b, U c) {
-
-
-
-
-}*/
-
-//template <typename T>.....
-//export 
-template <class T> T minimum(T a, T b) {
-
-	if (a < b)
-		return a;
-	else
-		return b;
-
-}
-
-//Specialisation de minimum pour char *
-const char* minimum(const char* adr1, const char* adr2) {
-
-	if (strcmp(adr1, adr2) < 0)
-		return adr1;
-	else
-		return adr2;
-
-
-}
-
-//minimum avec deux integer
-/*int minimum(int n_a, int n_b) {
-
-	if (n_a < n_b)
-		return n_a;
-	else
-		return n_b;
-
-}
-
-//minimum avec deux float
-
-float minimum(float flt_a,float flt_b) {
-
-
-	if (flt_a < flt_b)
-		return flt_a;
-	else
-		return flt_b;
-
-}*/
 
 
